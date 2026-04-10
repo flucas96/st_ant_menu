2028ee4d5afc84e43f553294435d318fd2801766
Fabian Lucas <59033065+flucas96@users.noreply.github.com>
2026-04-10 10:44:02 +0200

    fix: resolve css_styling param and bootstrap sourcemap errors (#1, #2) (#4)
    - #2: Add `css_styling` as alias for `css_styling_menu` so both the
      documented param name and the internal name work
    - #1: Remove `sourceMappingURL` reference from bootstrap.min.css that
      pointed to a non-existent .map file causing FileNotFoundError
    - Bump version to 0.0.18
    - Rebuild frontend with fix applied
    
    Co-authored-by: Fabian Lucas <fabian.lucas@eraneos.com>

---
+++ st_ant_menu/frontend/build/precache-manifest.05c7b8bb9328961b3aab59d9f6f17ff0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d1b3f155b2450329de44bc19c4ffe68",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c47d8de78b89311db37",
+    "url": "./static/js/2.eb109dfa.chunk.js"
+  },
+  {
+    "revision": "33d89f657bc3a602ef5b2b89d4b772a3",
+    "url": "./static/js/2.eb109dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d838f588c5f105da394",
+    "url": "./static/js/main.d3acb81e.chunk.js"
+  },
+  {
+    "revision": "23b81c6aad4ea3280af0",
+    "url": "./static/js/runtime-main.ac51b7c4.js"
+  }
+]);
